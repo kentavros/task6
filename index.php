dfc62fd7d5b72e4a5c57252cf50d28c411dbd18a
kentavros <assh4884@gmail.com>
2017-08-06 23:25:13 +0300

    two_worked

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+include ('libs/config.php');
+include ('libs/function.php');
 
 
 
 
+
+
+
+include ('template/tmp.php')
 ?>
---
+++ template/tmp.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Task 6 Music Band</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <!--<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
+    <!--[endif]-->
+</head>
+<body>
+<nav class="navbar navbar-inverse ">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand " href="#">Task 6 - Music</a>
+        </div>
+    </div>
+</nav>
+
+<div class="row" style="background: #fcf8d3;">
+    <div class="col-md-7 text-center" style="padding-top: 30px;">
+        test task 6dtest task 6dtest task 6dtest task 6d<br>
+        test task 6dtest task 6dtest task 6dtest task 6d<br>
+        test task 6dtest task 6dtest task 6dtest task 6d<br>
+
+    </div>
+    <div class="col-md-5 text-right" ">
+        <img src="img/music.jpg">
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<footer class="modal-footer navbar-inverse">
+    <div class="container">
+        <a class="navbar-brand" style="float: right" href="#">Task 6 - Good music will be eternal!</a>
+    </div>
+</footer>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
